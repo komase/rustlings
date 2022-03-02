2b6361e4b556e7e0b47828f813d42f0372539133
komase <eng00yoko@yahoo.co.jp>
2022-03-03 00:01:43 +0900

    dd

--- exercises/generics/generics3.rs
+++ exercises/generics/generics3.rs
@@ -9,21 +9,22 @@
 // to show that your changes allow alphabetical grades.
 
 // Execute 'rustlings hint generics3' for hints!
+use std::fmt::Display;
 
-// I AM NOT DONE
-
-pub struct ReportCard {
-    pub grade: f32,
+pub struct ReportCard<T> {
+    pub grade: T,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
+// https://doc.rust-jp.rs/book-ja/ch10-02-traits.html#%E3%83%88%E3%83%AC%E3%82%A4%E3%83%88%E5%A2%83%E7%95%8C%E3%82%92%E4%BD%BF%E7%94%A8%E3%81%97%E3%81%A6%E3%83%A1%E3%82%BD%E3%83%83%E3%83%89%E5%AE%9F%E8%A3%85%E3%82%92%E6%9D%A1%E4%BB%B6%E5%88%86%E3%81%91%E3%81%99%E3%82%8B
+impl<T: Display>  ReportCard<T> {
     pub fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
     }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -46,7 +47,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: "A+".to_string(),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
